b419bf919b481ed4a10e3c446dcf46fb1ffa3ecf
AndreiL <andrei.ledovskikh@network.global>
2023-03-31 13:42:46 +0400

    added reflection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,16 @@ package org.example;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-
-import java.io.IOException;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         Server server = ServerBuilder
                 .forPort(8181)
-                .addService(new HelloServiceImpl()).build();
+                .addService(new HelloServiceImpl())
+                .addService(ProtoReflectionService.newInstance())
+                .build();
 
         server.start();
         server.awaitTermination();
